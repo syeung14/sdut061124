433489c34b76189a2d5cf79a2351eb33d3925b64
venkats@agiledeveloper.com <venkats@agiledeveloper.com>
2024-06-11 14:47:15 +0200

    update

---
+++ code/part4/lab/calculatorexample/test/sample/CalculatorTest.java
@@ -0,0 +1,13 @@
+package sample;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CalculatorTest {
+  @Test
+  void canaryTest(){
+    assertTrue(true);
+  }
+
+}
